c7031e1fbfe4a2d900cf4f3c5a622fd7493cf6ad
busjee <busakbalazs@gmail.com>
2022-08-10 01:13:14 +0100

    Hover effect added while mouseover+mousedown

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const container = document.querySelector('.container');
 const board = document.querySelector('.board');
-let sizeNum = 16;
+let sizeNum = 32;
 // Function to create a number of divs in rows in which there are cells so it makes a square, using loops.
 function createGrid (sizeNum) {
-
     for(let i = 0; i < sizeNum; i++) {
         const row = document.createElement("div");
         row.classList.add('grid-row')
@@ -11,9 +10,11 @@ function createGrid (sizeNum) {
         for(let j = 0; j < sizeNum; j++) {
             const cell = document.createElement("div");
             cell.classList.add('grid-cell')
-            const cellSize = 960 / sizeNum
+            const cellSize = 480 / sizeNum
             cell.style.width = `${cellSize}px`
             cell.style.height = `${cellSize}px`
+            cell.addEventListener('mouseover', changeColor) // Event listener to hover when holding mouse down and moving at the same time
+            cell.addEventListener('mousedown', changeColor)
             row.appendChild(cell);
         }
         board.appendChild(row);
@@ -21,3 +22,14 @@ function createGrid (sizeNum) {
 }
 
 createGrid(sizeNum)
+
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+// Clicked hover effect function
+function changeColor(e) {
+    if (e.type === 'mouseover' && mouseDown) {
+        e.target.style.backgroundColor = 'black'
+    }
+}
